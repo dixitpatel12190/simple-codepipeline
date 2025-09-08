860ee288ca281eda94ba7aad466b32086f66fcca
Dixit Ranpariya <dixit.d.ranpariya@team.telstra.com>
2025-09-08 16:23:06 +0530

    first commit

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Update package lists
+sudo apt-get update
+# Install Nginx
+sudo apt-get install -y nginx
+# Start Nginx service
+sudo systemctl start nginx
+# Enable Nginx to start on boot
+sudo systemctl enable nginx
